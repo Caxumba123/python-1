efc400987def4afe16da6b67b0f9396feb8caa62
Carlos <carlos.villalba@escola.pr.gov.br>
2025-10-24 10:26:06 -0300

    Add files via upload

---
+++ tarefa2.py
@@ -0,0 +1,8 @@
+num_1 = float(input("Digite um numero: "))
+
+if num_1 > 0:
+    print("O numero é positivo.")
+elif num_1 < 0:
+    print("O numero é negativo.")
+else:
+    print("O numero é zero.")
