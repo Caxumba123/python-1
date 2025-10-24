7808152e4f8674b1e9dd9a939c638529eda5c61b
Carlos <carlos.villalba@escola.pr.gov.br>
2025-10-24 10:24:39 -0300

    Add files via upload

---
+++ tarefa1.py
@@ -0,0 +1,8 @@
+num_1 = input("Digite um numero: ")
+num_2 = input("Digite outro numero: ")
+if num_1 > num_2:
+   print("O primeiro numero é maior.")
+elif num_2 > num_1:
+   print("O segundo numero é maior.")
+else:
+   print("Os numeros são iguais")
